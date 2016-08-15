d6eef666dde0a84921aa25772d60c42ff6fd2e34
Eli Shieber <elishieber@college.harvard.edu>
2016-08-15 14:52:51 -0400

    added ProvCommand and PublishDatasetProv

---
+++ src/main/java/edu/harvard/iq/dataverse/PublishDatasetProv.java
@@ -0,0 +1,192 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.harvard.iq.dataverse;
+
+import java.util.List;
+import javax.ejb.EJB;
+
+
+
+/**
+ *
+ * @author eli
+ */
+
+//class for addition of prov on 
+public class PublishDatasetProv extends ProvCommand {
+    
+     @EJB
+     DataFileServiceBean datafileService;
+     
+    public static void msg(String str) {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println(str);
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+    
+
+    
+    // will be changed with the addition of datafile versioning
+    private void AddDatafileProv(String originator, String byAgent, String dsName, Dataset theDataset){
+        int dFnum = 0;
+        // iterates throught datafiles in dataset, if there is already an agent
+        // responsible than do nothing, else new attribution edge
+        // 
+        
+        Float[] datasetVersionArray = [];
+        
+        Integer datafileMetadataSize = datafile.getFileMetadatas().size();
+        List<FileMetadata> datafileMetadataList = datafile.getFileMetadatas();
+        for (Integer i = 0; i < datafileMetadataSize; i++){
+        
+        }
+     
+        for (DataFile datafile: theDataset.getFiles());
+            
+            DataFile currentDatafile = datafileService.find(datafile.getId());
+            String storageId = currentDatafile.getStorageIdentifier();
+            
+            CPLObject createdDatafile = pF.createEntity(storageId);
+            
+            if(datafile.getPublicationDate() == null){
+ 
+                CPLEdge isMemberOf = pF.createIsMemberOf(dsName, storageId);
+                
+                CPLEdge wasAttrTo = pF.createWasAttributedTo(StorageId, byAgent);
+                
+            }
+            else if(pF.CPLObject.lookup(originator, storageId, "activity") != null) { 
+                
+                Long curDatasetVersion = datafile.getFileMetadatas().get(dFnum).getDatasetVersion();
+                
+                // going to have to iterate through all of the versions to find the previous one :`(
+                
+                CPLEdge isMemberOf = pF.createIsMemberOf(dsName, storageId);
+                
+                CPLEdge wasAttrTo = pF.createWasAttributedTo(StorageId, byAgent);
+                
+                CPLEdge wasGenBY = pF.createWasGeneratedBy();
+                
+                dFnum++;
+                
+                //from dataset version get datset
+                //datset has a list of dataset versions
+                //sort by major version/ minor version number
+                //iterate through
+              
+            
+            }
+            else {
+            
+            }
+            
+            //CPLObject createdDatafile1 = pF.createEntity(storageId);
+            
+           
+            //if (pF.CPLObject.lookup(originator, storageId, "activity") == null) {
+            //    CPLEdge wasAttrTo = pF.createWasAttributedTo(byAgent, storageId);
+            
+            
+            //Integer datafileMetadataSize = datafile.getFileMetadatas().size();
+            //List<FileMetadata> datafileMetadataList = datafile.getFileMetadatas();
+            for (Integer i = 0; i < datafileMetadataSize; i++){
+                //accessing metadata
+                
+                
+                
+                
+                //String storageId = currentDatafile.getStorageIdentifier();
+
+                
+                
+                
+                
+                msg(datafile.getFileMetadatas().get(i).getLabel());
+                msg(datafile.getFileMetadatas().get(i).getDescription());
+                msg(Long.toString(datafile.getFileMetadatas().get(i).getDatasetVersion().getMinorVersionNumber()));
+                msg(Long.toString(datafile.getFileMetadatas().get(i).getDatasetVersion().getVersionNumber()));
+                msg(Boolean.toString(datafile.isRestricted()));
+            }
+                
+            }
+            //
+            
+            msg("DF" + dFnum + "storage identifier: " + dataFile.getStorageIdentifier());
+            
+            dFnum++;
+    }
+        /*
+        DATAFILE VERSIONING PSUEDOCODE
+
+        on creation of new dataset iterate through datafiles
+
+        in datafile record will see if is a new version
+
+        if it is get the ID of that version and trace it back to the prev version
+
+        draw the is replacement line
+
+        */
+    
+    
+    
+    // creation of provenance for either a completely new dataset, or a new version of a dataset
+    public static  void createProv(String originator, String name, String agent, String versionTransformation, String versionNumber, Dataset theDataset){
+        
+        if (versionTransformation == null){
+        
+            ProvFactory pF = new ProvFactory(originator);
+            pF.setBundle(originator, name);
+
+            CPLObject createdDataset = pF.createEntity(name);
+            CPLObject byAgent = pF.createAgent(agent);
+
+            CPLEdge wasAttrTo = pF.createWasAttributedTo(byAgent, createdDataset);
+
+            addDatafileProv(originator, agent, name, theDataset);
+        }
+
+        //if the dataset is being created for the first time
+        if (versionNumber != "1.0" || versionTransformation == null){
+            CPLObject vrsTransformation = pF.createActivity(versionTransformation); 
+            CPLEdge wasAssociatedWith = pF.createWasAssociatedWith(vrsTransformation, agent);
+            
+            // TODO: go into metadata to get the previous version
+            CPLEdge wasUsedBy = pF.createWasUsedBy(vrsTransformation, prevDataset);
+            CPLEdge wasGeneratedBy = pF.createWasGeneratedBy(createdDataset, vrsTransformation);
+        }
+        
+        msg("version: "+theDataset.getLatestVersion()); 
+        msg("versionMajor: "+theDataset.getVersionNumber());
+        msg("versionMinor: "+theDataset.getMinorVersionNumber()); 
+        
+    }
+    
+    // creation of provenance for either the linking of 2 problem sets via transformation
+    public static void createProv(String originator, String name, String agent, String parentName, String versionNumber, String datasetTransformation, Dataset theDataset){
+
+        // add a parentOriginator or is that redundant?
+        // probably yes, but leave it so that in the future allow for
+        // communication between installations
+        
+        ProvFactory pF = new ProvFactory(originator);
+        pF.setBundle(originator, name);
+
+        CPLObject parentDataset = CPLObject.lookup(originator, parentName, "entity");
+        CPLObject createdDataset = pF.newEntity(name);
+        CPLObject byAgent = pF.createAgent(agent);
+        
+        CPLEdge wasAttrTo = pF.createWasAttributedTo(byAgent, createdDataset);
+        CPLEdge usd = pF.createUsed(parentName, datasetTransformation);
+        CPLEdge wasGenBy = pF.createWasGeneratedBy(datasetTransformation, parentName);
+        
+        addDatafileProv(originator, agent, theDataset);
+        
+        
+    }  
+
+    
+}
